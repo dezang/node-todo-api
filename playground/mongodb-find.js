9bc061938cbc419d333f28e0060409ddaec826c6
dezang <dezang@dezang.net>
2016-12-11 17:06:23 +0900

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+const user = {name: 'Dezang', age: 30};
+const {name} = user;
+console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('584d03150224cdad42c0f2f0')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Dezang'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  })
+
+  // db.close();
+});
